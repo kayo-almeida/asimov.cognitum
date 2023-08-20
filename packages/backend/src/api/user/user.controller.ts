f06400fe0e2a9f067ffba52f89937874b3e28cd9
Kayo Almeida <kayo.almeida.dev@gmail.com>
2023-08-20 09:53:25 -0300

    feat: add update user endpoint

--- packages/backend/src/api/user/user.controller.ts
+++ packages/backend/src/api/user/user.controller.ts
@@ -1,9 +1,21 @@
 import { appSettings } from '@config/app-settings';
 import { UserService } from '@infra/service/user.service';
-import { Body, Controller, Get, Injectable, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Injectable,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUserParamRequestDto } from '@api/user/dtos/requests/get-user.params';
 import { CreateUserBodyRequestDto } from '@api/user/dtos/requests/create-user.params';
+import {
+  UpdateUserBodyRequestDto,
+  UpdateUserParamRequestDto,
+} from '@api/user/dtos/requests/update-user.params';
 
 @Injectable()
 @Controller({ version: appSettings.app.version })
@@ -28,6 +40,18 @@ export class UserController {
     @Body() payload: CreateUserBodyRequestDto,
   ): Promise<unknown> {
     const user = await this.service.createUser(payload);
+
+    return user;
+  }
+
+  @Put('/user')
+  @ApiTags('user')
+  @ApiOperation({ summary: 'Update a user' })
+  public async updateUser(
+    @Param() routeParam: UpdateUserParamRequestDto,
+    @Body() payload: UpdateUserBodyRequestDto,
+  ): Promise<unknown> {
+    const user = await this.service.updateUser(routeParam.userId, payload);
 
     return user;
   }
--- packages/backend/src/infra/service/user.service.ts
+++ packages/backend/src/infra/service/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   constructor(private readonly repository: UserRepository) {}
 
   async findUser(_id: string): Promise<UserModel | null> {
-    console.debug('findUser(id)', _id);
+    console.debug('findUser(_id)', _id);
 
     const result = await this.repository.findOne({ _id });
 
@@ -26,8 +26,23 @@ export class UserService {
       _id: uuid(),
     });
 
-    console.debug('createPolicy result: {addResult}', {
-      result: result,
+    console.debug('createPolicy result:', {
+      result,
+    });
+
+    return result;
+  }
+
+  async updateUser(
+    _id: string,
+    user: Omit<IUser, '_id'>,
+  ): Promise<UserModel | null> {
+    console.debug('updateUser({_id, user})', { _id, user });
+
+    const result = await this.repository.update({ _id }, user);
+
+    console.debug('updateUser result:', {
+      result,
     });
 
     return result;
